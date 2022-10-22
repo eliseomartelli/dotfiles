fc79a5663153eca4588c1ca4fa8e7ca210fe9b7d
Eliseo Martelli <me@eliseomartelli.it>
2022-10-22 14:36:00 +0200

    fix: simplify treesitter cfg

--- config/nvim/after/plugin/treesitter.rc.lua
+++ config/nvim/after/plugin/treesitter.rc.lua
@@ -3,28 +3,14 @@ local ts = require("nvim-treesitter.configs")
 ts.setup {
   highlight = {
     enable = true,
-    disable = {},
   },
   indent = {
     enable = true,
-    disable = {},
-  },
-  ensure_installed = {
-    "tsx",
-    "toml",
-    "fish",
-    "php",
-    "json",
-    "yaml",
-    "swift",
-    "css",
-    "html",
-    "lua"
   },
   autotag = {
     enable = true,
   },
 }
 
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+-- local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+-- parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
