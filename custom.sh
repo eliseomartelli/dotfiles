d3d5912b6734caa1cab73ad787eb9db6a2441696
Eliseo Martelli <me@eliseomartelli.it>
2022-10-23 23:40:58 +0200

    feat: add more custom things

--- custom.sh
+++ custom.sh
@@ -7,10 +7,16 @@ defaults write -g NSWindowShouldDragOnGesture -bool true
 defaults write com.apple.dock autohide-delay -float 0
 
 # Speed up animation of dock hiding.
-defaults write com.apple.dock autohide-time-modifier -float 0.3
+defaults write com.apple.dock autohide-time-modifier -float 0.25
+
+# Set dock icon size.
+defaults write com.apple.dock tilesize -int 32
 
 # Show hidden apps on dock.
 defaults write com.apple.Dock showhidden -bool yes
+
+# Set dock autohide.
+defaults write com.apple.dock autohide -bool true
 
 # Set screenshot location to ~/Screenshots.
 mkdir -p ~/Screenshots
@@ -35,3 +41,14 @@ defaults write com.apple.dock scroll-to-open -bool true
 # Set plain text default textedit file.
 defaults write com.apple.TextEdit RichText -bool false
 
+# Set default search scope of finder to current folder.
+defaults write com.apple.finder "FXDefaultSearchScope" -string "SCcf"
+
+# Remove items from trash after 30 day.
+defaults write com.apple.finder "FXRemoveOldTrashItems" -bool true
+
+# Disable delay in Finder Toolbar to show icon.
+defaults write NSGlobalDomain "NSToolbarTitleViewRolloverDelay" -float "0"
+
+# Set date in menubar.
+defaults write com.apple.menuextra.clock "DateFormat" -string "\"EEE d\""
