9cde0006c8ca3cd2b8e916d264c08bad1dbe3928
Eliseo Martelli <me@eliseomartelli.it>
2024-07-08 10:08:17 +0200

    feat: configure tsserver and denols

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -240,6 +240,15 @@ lspconfig.lua_ls.setup({
   }
 })
 
+lspconfig.denols.setup {
+  root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+}
+
+lspconfig.tsserver.setup {
+  root_dir = lspconfig.util.root_pattern("package.json"),
+  single_file_support = false
+}
+
 -- mini.clue.
 require("which-key").setup()
 
