68c6e7bbdc6c312c370cc68822e51af2ed4d2749
Eliseo Martelli <me@eliseomartelli.it>
2024-09-10 11:06:41 +0200

    fix: change tsserver name

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -246,7 +246,7 @@ lspconfig.denols.setup {
   root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
 }
 
-lspconfig.tsserver.setup {
+lspconfig.ts_ls.setup {
   root_dir = lspconfig.util.root_pattern("package.json"),
   single_file_support = false
 }
